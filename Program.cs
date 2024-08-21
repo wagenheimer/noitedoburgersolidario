5984b9996ab779b21ee1a275052ab39798fd2003
Cezar Augusto Ximenes Wagenheimer Lima <wagenheimer@gmail.com>
2024-08-21 12:20:25 -0300

    Ajustes Nome Projeto

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using Noite_do_Burger_Solidário;
+using NoiteBurgerSolidario;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
