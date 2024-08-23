1e6dc6d2c63f35af81317591ff3028e25cb5c50a
Cezar Augusto Ximenes Wagenheimer Lima <wagenheimer@gmail.com>
2024-08-23 14:50:07 -0300

    Adicionado Patrocinadores

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +11,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorise(options =>
+{
+    options.Immediate = true;
+}).AddBootstrap5Providers();
 
 await builder.Build().RunAsync();
